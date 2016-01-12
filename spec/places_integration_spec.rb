cedf55d82b89153ccd5dfaee8cc2bad7a5373abd
Krystan Menne and Riley Starnes <rstarnes9@gmail.com>
2016-01-12 11:33:58 -0800

    refactor tests to DRY methods

--- spec/places_integration_spec.rb
+++ spec/places_integration_spec.rb
@@ -6,8 +6,7 @@ set(:show_exceptions, false)
 describe('place addition path', {:type => :feature}) do
   it('processes the user input and redirects to success page') do
     visit('/')
-    fill_in('place_name', :with => "Moon")
-    click_button('Add to Place List')
+    fill_in_entry_and_add_to_list()
     expect(page).to(have_content('Your place has been added!'))
   end
 end
@@ -15,9 +14,7 @@ end
 describe('place list render path', {:type => :feature}) do
   it('renders the place list on the index page') do
     visit('/')
-    fill_in('place_name', :with => "Moon")
-    click_button('Add to Place List')
-    click_link('Back')
+    add_entry_then_click_back()
     expect(page).to(have_content('Moon'))
   end
 end
@@ -25,20 +22,34 @@ end
 describe('place list clear path', {:type => :feature}) do
   it('clears the place list on the index page') do
     visit('/')
-    fill_in('place_name', :with => "Moon")
-    click_button('Add to Place List')
-    click_link('Back')
-    click_button('Clear Place List')
+    add_entry_then_click_back()
+    clear_place_list()
     expect(page).to(have_content('Place list cleared'))
   end
 
   it('clears the place list on the index page') do
     visit('/')
-    fill_in('place_name', :with => "Moon")
-    click_button('Add to Place List')
-    click_link('Back')
-    click_button('Clear Place List')
-    click_link('Back')
+    add_entry_then_clear()
     expect(page).not_to(have_content('Moon'))
   end
+end
+
+define_method(:fill_in_entry_and_add_to_list) do
+  fill_in('place_name', :with => "Moon")
+  click_button('Add to Place List')
+end
+
+define_method(:add_entry_then_click_back) do
+  fill_in_entry_and_add_to_list()
+  click_link('Back')
+end
+
+define_method(:clear_place_list) do
+  click_button('Clear Place List')
+end
+
+define_method(:add_entry_then_clear) do
+  add_entry_then_click_back()
+  clear_place_list()
+  click_link('Back')
 end
